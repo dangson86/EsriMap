eb2f86ea57848a5e86773095783147c529dac153
Son Dang <sdang@ensiteusa.com>
2021-04-09 11:39:42 -0500

    update to angular 11

--- projects/esri-map/src/modules/map/components/map-toc-ui/map-toc-ui.component.spec.ts
+++ projects/esri-map/src/modules/map/components/map-toc-ui/map-toc-ui.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MapTocUIComponent } from './map-toc-ui.component';
 
@@ -6,7 +6,7 @@ describe('MapTocUIComponent', () => {
   let component: MapTocUIComponent;
   let fixture: ComponentFixture<MapTocUIComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MapTocUIComponent ]
     })
--- src/app/components/test-map/test-map.component.spec.ts
+++ src/app/components/test-map/test-map.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TestMapComponent } from './test-map.component';
 
@@ -6,7 +6,7 @@ describe('TestMapComponent', () => {
   let component: TestMapComponent;
   let fixture: ComponentFixture<TestMapComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TestMapComponent ]
     })
