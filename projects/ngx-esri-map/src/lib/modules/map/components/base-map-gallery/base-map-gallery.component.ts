68dc5b1b8bf8e1361f18c36a00a2676147637a58
Son Dang <sdang@ensiteusa.com>
2023-01-27 15:57:28 -0600

    misc bug fix

--- projects/ngx-esri-map/src/lib/modules/map/components/base-map-gallery/base-map-gallery.component.ts
+++ projects/ngx-esri-map/src/lib/modules/map/components/base-map-gallery/base-map-gallery.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
 import { Subscription } from 'rxjs';
 import { MapViewComponent } from '../map-view/map-view.component';
@@ -7,33 +7,34 @@ import { MapViewComponent } from '../map-view/map-view.component';
   templateUrl: './base-map-gallery.component.html',
   styleUrls: ['./base-map-gallery.component.scss']
 })
-export class BaseMapGalleryComponent implements OnInit, OnDestroy, AfterViewInit {
+export class BaseMapGalleryComponent implements OnInit, OnDestroy {
   sub: Subscription;
 
-  @ViewChild("basemapWidget") basemapWidget: ElementRef;
-  constructor(private host: MapViewComponent) { }
+  private basemapGallery: BasemapGallery;
+  constructor(private mapComp: MapViewComponent, private host: ElementRef) { }
 
   ngOnDestroy(): void {
     if (this.sub) {
       this.sub.unsubscribe();
     }
+    if (this.basemapGallery) {
+      this.basemapGallery.destroy();
+    }
   }
 
   ngOnInit(): void {
-
-  }
-  ngAfterViewInit(): void {
-    this.sub = this.host.initMap$.pipe().subscribe({
+    this.sub = this.mapComp.initMap$.pipe().subscribe({
       next: initModel => {
         let view = initModel.mapView;
-        const basemapGallery = new BasemapGallery({
+        this.basemapGallery = new BasemapGallery({
           view: view,
-          container: this.basemapWidget.nativeElement
+          container: this.host.nativeElement
         });
 
-        basemapGallery.render();
+        this.basemapGallery.render();
       }
     });
   }
+
 
 }
